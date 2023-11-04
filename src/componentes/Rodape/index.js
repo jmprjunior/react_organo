bbcd20d543f2f28298c4db4346f1a251439ce509
Júnior Rodrigues <junior@juniorrodrigues.eti.br>
2023-11-03 21:35:17 -0300

    Add files via upload

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,23 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <div className="social">
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/fb.png" alt="" />
+                </a>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/tw.png" alt="" />
+                </a>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/ig.png" alt="" />
+                </a>
+            </div>
+            <img src="/imagens/logo.png" alt="Banner principal da página do Organo" />
+            <h3>Desenvolvido por Alura.</h3>
+        </footer>
+    )
+}
+
+export default Rodape;
